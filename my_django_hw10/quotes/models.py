04f5aae6bfcc4df7d920aff959be9fb6821e8b32
Roman_M <prominb@gmail.com>
2024-11-02 04:17:56 +0200

    e005 add models, registr in admin

--- my_django_hw10/quotes/models.py
+++ my_django_hw10/quotes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    fullname = models.CharField(max_length=50)
+    born_date = models.CharField(max_length=50)
+    born_location = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.fullname}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=35, null=False, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Quote(models.Model):
+    quote = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # return f"{self.quote}, {self.author}, {self.tags}"
+        return f"{self.quote} - {self.author}"
