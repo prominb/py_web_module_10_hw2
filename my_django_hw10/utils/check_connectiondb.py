3f38d1d455611ce95ab266373c1678b8ecf47275
Roman_M <prominb@gmail.com>
2024-11-08 00:24:43 +0200

    e010 edit check_conn file

--- my_django_hw10/utils/check_connectiondb.py
+++ my_django_hw10/utils/check_connectiondb.py
@@ -6,12 +6,16 @@ from pymongo import MongoClient
 
 
 # Перетворення відносного шляху в абсолютний
-relative_path = Path("check_connectiondb.py")
+# relative_path = Path("check_connectiondb.py")
+relative_path = Path().cwd()
+# print(relative_path)
 absolute_path = relative_path.absolute()
+# print(absolute_path.parent)
 # print(absolute_path.parent.parent.parent)
-path_to_env = absolute_path.parent.parent.parent / ".env"
+# path_to_env = absolute_path.parent.parent.parent / ".env"
+path_to_env = absolute_path.parent / ".env"
 # print(path_to_env)
-# print("Exists:", path_to_env.exists())
+print("DOT_ENV file Exists? ->", path_to_env.exists())
 if path_to_env:
     config = dotenv_values(path_to_env)
     # print(type(config))
