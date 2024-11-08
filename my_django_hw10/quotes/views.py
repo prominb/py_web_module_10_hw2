5f842d2fa6c03b4af4d38b08b11fd5e752d9ce8b
Roman_M <prominb@gmail.com>
2024-11-08 05:01:17 +0200

    e019 add author start

--- my_django_hw10/quotes/views.py
+++ my_django_hw10/quotes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 
 from django.http import HttpResponse
 
 from .utils import get_mongodb
-from .models import Tag, Quote
+from .models import Tag, Quote, Author
 from .forms import TagForm, QuoteForm
 
 # Create your views here.
@@ -49,7 +49,9 @@ def add_quote(request):
 
     return render(request, 'quotes/add_quote.html', {"tags": tags, 'form': QuoteForm()})
 
-
+def author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'quotes/author.html', {"author": author})
 
 
 
