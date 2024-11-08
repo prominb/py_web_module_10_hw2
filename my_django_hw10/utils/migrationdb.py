cc4df140a8267084cc086dafcf063644a8be6a60
Roman_M <prominb@gmail.com>
2024-11-08 02:51:44 +0200

    e013 done Migrationdb to postgres

--- my_django_hw10/utils/migrationdb.py
+++ my_django_hw10/utils/migrationdb.py
@@ -1,15 +1,15 @@
 import os
 # from pathlib import Path
 
-# import django
+import django
 from dotenv import load_dotenv  #, dotenv_values
 from pymongo import MongoClient
 
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hw10_quotes.settings")
-# django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hw10_quotes.settings")
+django.setup()
 
-# from quotes.models import Author, Tag, Quote  # noqa
+from quotes.models import Author, Tag, Quote  # noqa
 
 
 load_dotenv()
@@ -32,34 +32,34 @@ db = client[MONGO_DB_NAME]
 authors = db.authors.find()
 
 for author in authors:
-    print(author)
-    # Author.objects.get_or_create(
-    #     fullname=author['fullname'],
-    #     born_date=author['born_date'],
-    #     born_location=author['born_location'],
-    #     description=author['description']
-    # )
+    # print(author)
+    Author.objects.get_or_create(
+        fullname=author['fullname'],
+        born_date=author['born_date'],
+        born_location=author['born_location'],
+        description=author['description']
+    )
 
-# quotes = db.quotes.find()
+quotes = db.quotes.find()
 
-# for quote in quotes:
-#     # print(quote['tags'])
-#     tags = []
-#     for tag in quote['tags']:
-#         # print(tag)
-#         t, *_ = Tag.objects.get_or_create(name=tag)
-#         tags.append(t)
-#     # print(tags)
+for quote in quotes:
+    # print(quote['tags'])
+    tags = []
+    for tag in quote['tags']:
+        # print(tag)
+        t, *_ = Tag.objects.get_or_create(name=tag)
+        tags.append(t)
+    # print(tags)
 
-#     exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
+    exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
 
-#     if not exist_quote:
-#         author = db.authors.find_one({'_id': quote['author']})
-#         a = Author.objects.get(fullname=author['fullname'])
-#         q = Quote.objects.create(
-#             quote=quote['quote'],
-#             author=a
-#         )
+    if not exist_quote:
+        author = db.authors.find_one({'_id': quote['author']})
+        a = Author.objects.get(fullname=author['fullname'])
+        q = Quote.objects.create(
+            quote=quote['quote'],
+            author=a
+        )
 
-#         for tag in tags:
-#             q.tags.add(tag)
+        for tag in tags:
+            q.tags.add(tag)
